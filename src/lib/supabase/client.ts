05cff072534bbda88d6ca467f5a254d6e8e0c33a
yejiLee <dpwlals1017@gmail.com>
2026-06-09 00:05:51 +0900

    Refactor: Supabase 클라이언트에 Database 타입 적용

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,7 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+import type { Database } from "@/lib/supabase";
 
 export const createClient = () =>
-  createBrowserClient(supabaseUrl!, supabaseKey!);
+  createBrowserClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  );
